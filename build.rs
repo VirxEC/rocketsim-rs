b6a05a7fc0fe5ef971fed278594864126dbc8752
VirxEC <virx@virxcase.dev>
2023-05-05 00:18:35 -0400

    Remove references to lto in build file

--- build.rs
+++ build.rs
@@ -3,16 +3,7 @@ use glob::glob;
 use miette::{IntoDiagnostic, Result};
 
 fn main() -> Result<()> {
-    let is_debug = std::env::var("PROFILE").into_diagnostic()?.as_str() == "debug";
-
-    let clang_args = if is_debug { ["-std=c++20"].as_slice() } else { ["-std=c++20", "-flto"].as_slice() };
-
-    let mut builder = Builder::new("src/lib.rs", ["RocketSim/src/", "arenar/"]).extra_clang_args(clang_args).build()?;
-
-    // A bug in AutoCXX prevents us from being able to use LTO
-    // if !is_debug {
-    //     builder.flag_if_supported("-flto").flag_if_supported("/GL");
-    // }
+    let mut builder = Builder::new("src/lib.rs", ["RocketSim/src/", "arenar/"]).extra_clang_args(&["-std=c++20"]).build()?;
 
     builder
         .use_plt(false)
