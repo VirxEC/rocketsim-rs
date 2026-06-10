d9972297fc8ac4b41f665187077a65a2690f6600
VirxEC <virx@virxcase.dev>
2026-06-10 12:31:06 -0500

    Format `flat.rs`

--- build.rs
+++ build.rs
@@ -8,6 +8,47 @@ const SCHEMA_DIR: &str = "./spec";
 const OUT_FILE: &str = "./src/flat.rs";
 
 #[cfg(feature = "bin")]
+fn format_string(s: &str) -> Result<String, String> {
+    use std::io::Write;
+    use std::process::{Command, Stdio};
+
+    let mut child = Command::new("rustfmt");
+
+    child
+        .arg("--edition=2024")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+
+    let mut child = child
+        .spawn()
+        .map_err(|err| format!("Unable to spawn rustfmt. Perhaps it is not installed? {err}"))?;
+
+    {
+        let child_stdin = child.stdin.as_mut().unwrap();
+        child_stdin
+            .write_all(s.as_bytes())
+            .map_err(|err| format!("Unable to write the file to rustfmt: {err}"))?;
+    }
+
+    let output = child
+        .wait_with_output()
+        .map_err(|err| format!("Unable to get the formatted file back from rustfmt: {err}"))?;
+
+    if output.status.success() && output.stderr.is_empty() {
+        Ok(String::from_utf8_lossy(&output.stdout).into_owned())
+    } else if output.stderr.is_empty() {
+        Err(format!("rustfmt failed with exit code {}", output.status))
+    } else {
+        Err(format!(
+            "rustfmt failed with exit code {} and message:\n{}",
+            output.status,
+            String::from_utf8_lossy(&output.stderr).into_owned(),
+        ))
+    }
+}
+
+#[cfg(feature = "bin")]
 fn generate_flatbuffers() {
     use std::fs;
 
@@ -18,8 +59,9 @@ fn generate_flatbuffers() {
     let raw_out = planus_codegen::generate_rust(&declarations)
         .unwrap()
         .replace("#[no_implicit_prelude]\n", "");
+    let formatted_out = format_string(&raw_out).unwrap();
 
-    fs::write(OUT_FILE, raw_out.as_bytes()).unwrap();
+    fs::write(OUT_FILE, formatted_out.as_bytes()).unwrap();
 }
 
 fn main() {
